907f928720d3aa9478986b6eb46264ed88524ddf
jjackson <jeremy@bluewall.com>
2017-07-18 16:52:20 -0500

    Initial commit

---
+++ src/DocBlockParser.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+import {ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, window} from 'vscode';
+
+/**
+ * Inital Class for parsing Doc Block comments
+ */
+export class DocBlockParser {
+
+  /**
+   * Parses function and generates doc block for said function
+   * 
+   * @param  {TextDocument} doc The content of the editor
+   * 
+   * @return {void} 
+   */
+  public parseFunction(doc: TextDocument):void {
+    let doc = window.activeTextEditor;
+    let docContent = doc.getText();
+  }
+}
---
+++ src/extension.ts
@@ -0,0 +1,24 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, 
+  StatusBarItem, TextDocument}    from 'vscode';
+import {DocBlockParser}           from './DocBlockParser';
+
+export function activate(context: ExtensionContext) {
+  // create a new word counter
+  let docBlockParser = new DocBlockParser();
+  
+  let disposable = commands.registerCommand('extension.parseFunction', () => {
+    let document = window.activeTextEditor.document;
+    let language = '';
+    docBlockParser.parseFunction(document);
+  });
+
+  // Add to a list of disposables which are disposed when this extension is 
+  // deactivated.
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
