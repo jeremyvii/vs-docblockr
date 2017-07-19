786dce0a30af697bb98a004c9468a87f135d35b1
jjackson <jeremy@bluewall.com>
2017-07-19 17:07:25 -0500

    Log current active cursor position

--- src/DocBlockParser.ts
+++ src/DocBlockParser.ts
@@ -1,11 +1,13 @@
 'use strict';
 
-import {ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, window} from 'vscode';
+import {ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, 
+  window, Disposable, SnippetString, Position} from 'vscode';
 
 /**
  * Inital Class for parsing Doc Block comments
  */
 export class DocBlockParser {
+  private _disposable: Disposable;
 
   /**
    * Parses function and generates doc block for said function
@@ -17,6 +19,14 @@ export class DocBlockParser {
   public parseFunction(doc: TextDocument): void { 
     if (doc.languageId == 'javascript') {
       let docContent = doc.getText();
+      let subcriptions: Disposable[] = [];
+      let position = window.activeTextEditor.selections[0].active;
+      console.log(position);
+      window.showInformationMessage(docContent);
     }
+  }
+
+  dispose() {
+    this._disposable.dispose();
   }
 }
