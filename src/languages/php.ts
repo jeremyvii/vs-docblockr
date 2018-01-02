aa72e56cba3fee98f9260d0a4b6a724c55f41f1b
Jeremy Jackson <git@jeremyvii.com>
2018-01-02 15:34:51 -0600

    Tokenize functions and classes for PHP

--- src/languages/php.ts
+++ src/languages/php.ts
@@ -29,7 +29,7 @@ export class PHP extends Parser {
       grammer: {
         function: 'function',
         class: 'class',
-        modifiers: ['const', 'let', 'var'],
+        modifiers: ['public', 'protected', 'private'],
         identifier: '[a-zA-Z_$0-9]'
       }
     });
@@ -56,63 +56,25 @@ export class PHP extends Parser {
     if (code !== undefined) {      
       // Lex code string provided
       let lexed = this.lexer(code);
-      console.log(lexed);
       // Get current line position
       let current = this.findByType('text', lexed);
       // Get end of line position
       let eos = this.findByType('eos', lexed);
       // Create shortcut to indentifier string
       let indentifier = this.settings.grammer.identifier;
-      // Create regular expression for finding function prototypes
-      // let regex = new RegExp('(' + indentifier + '+)\.prototype\.(' + indentifier + '+)');
-      // // Get code lexed object if exists this is used for variable blocks
-      // let codeLexed = this.findByType('code', lexed);
-      // // Check if we have gotten a token value
-      // if (this.matchesGrammer(lexed[0].val, 'function') ||
-      //     this.matchesGrammer(lexed[0].val, 'class')) {
-      //   // Append matched token to token type
-      //   tokens.type = lexed[0].val;
-      //   // The next time this function is ran,
-      //   // indicate that it should expect a name
-      //   next = lexed[0].val;
-      // // Add special case for prototype functions
-      // } else if (regex.test(code)) {
-      //   // Get regular expression result
-      //   let result = regex.exec(code);
-      //   // Indicate we have a function in our token
-      //   tokens.type = this.settings.grammer.function;
-      //   // Set function name
-      //   tokens.name = result[2];
-      //   // Clean malformed input to prevent errors in the Pug Lexer
-      //   current.val = current.val.replace('= ', '');
-      // // Get variable properties
-      // } else if (codeLexed) {
-      //   // Set token name
-      //   tokens.name = lexed[0].val;
-      //   // Set token type
-      //   tokens.type = 'variable';
-      //   // Return token as is
-      //   return tokens;
-      // // Check for function modifiers let, var, etc.
-      // } else if (this.matchesGrammer(lexed[0].val, 'modifiers')) {
-      //   // Create regular expression object for finding function variables
-      //   let funcRegex = new RegExp('(' + indentifier + '+) = (' + this.settings.grammer.function + ')');
-      //   // Check if regular expression matches code next up to lexed
-      //   if (funcRegex.test(current.val)) {
-      //     // Get matches from regular expression
-      //     let result = funcRegex.exec(current.val);
-      //     // Get function parameters from string
-      //     let params = current.val.replace(result[1] + ' = ' + result[2], '');
-      //     // Swap function name and statement to prevent pug lexer errors
-      //     current.val = result[2] + ' ' + result[1] + params;
-      //   } else {
-      //     // Strip spaces from code to help pug lexer
-      //     current.val = current.val.replace(' = ', '=').replace(';', '');
-      //   }
-      // } else if (this.matchesGrammer(next)) {
-      //   // Set the tokens name
-      //   tokens.name = lexed[0].val;
-      // }
+      // Check if we have gotten a token value
+      if (this.matchesGrammer(lexed[0].val, 'function') ||
+          this.matchesGrammer(lexed[0].val, 'class')) {
+        // Append matched token to token type
+        tokens.type = lexed[0].val;
+        // The next time this function is ran,
+        // indicate that it should expect a name
+        next = lexed[0].val;
+      // Set block name
+      } else if (this.matchesGrammer(next)) {
+        // Set the tokens name
+        tokens.name = lexed[0].val;
+      }
       // Check for any parameters in lexed array by checking for a start
       // attribute type
       if (this.findByType('start-attributes', lexed)) {
