25a8cd1933417d812ae08fd2921c452aa231e150
Jeremy Jackson <git@jeremyvii.com>
2018-07-15 15:46:01 -0500

    Wrap types in {} to conform to JSDoc

--- src/languages/javascript.ts
+++ src/languages/javascript.ts
@@ -183,4 +183,58 @@ export class JavaScript extends Parser {
     }
     return tokens;
   }
+
+  /**
+   * Renders parameter tag template for docblock. This method is
+   * being overwritten in order to wrap `{}` around binding types
+   * 
+   * Arguments c, t, p should be assumed to be computed by `renderParamTags()`.
+   * These ambigious argument names simply refer to the spaces between columns.
+   * 
+   * @param   {string}  c     Spaces computed between inital tag and param type
+   * @param   {string}  type  The variable type of said parameter
+   * @param   {string}  t     Spaces computed between param type and param name
+   * @param   {string}  name  Parameter's name binding 
+   * @param   {string}  p     Spaces computed between param name and description 
+   * @param   {string}  desc  Describes the parameter
+   * 
+   * @return  {string}        Rendered parameter tag 
+   */
+  public getParamTag(
+    c: string,
+    type: string,
+    t: string,
+    name: string,
+    p: string,
+    desc: string): string {
+    return `@param${c} {${type}}${t}${name}${p}${desc}`;
+  }
+
+  /**
+   * Renders return tag with return type and computed spacing. This method is
+   * being overwritten in order to wrap `{}` around binding types
+   * 
+   * @param   {string}  columns  Computed spaces between tag and type 
+   * @param   {string}  type     Type associated with return value (in docblock 
+   *                             not this method)
+   * 
+   * @return  {string}           Rendered return tag 
+   */
+  public getReturnTag(columns: string, type: string): string {
+    return `@return${this.columns}{${type}}`;
+  }
+
+  /**
+   * Renders var tag with property type and computed spacing. This method is 
+   * being overwritten in order to wrap `{}` around binding types 
+   * 
+   * @param   {string}  columns  Computed spaces between tag and type 
+   * @param   {string}  type     Type associated with property value (in docblock
+   *                             not this method)
+   * 
+   * @return  {string}           Rendered property tag 
+   */
+  public getVarTag(columns: string, type: string): string {
+    return `@var${this.columns}{${type}}`;
+  }
 }
