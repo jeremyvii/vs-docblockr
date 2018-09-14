7ee06087d65555fc39dd08943d632fe2e271c854
Jeremy Jackson <jeremy@bluewall.com>
2018-09-14 12:07:41 -0500

    Remove trailing whitespace

--- src/snippets.ts
+++ src/snippets.ts
@@ -1,22 +1,22 @@
 /**
- * Handles snippet auto-completion, and acts as a layer between visual studio 
+ * Handles snippet auto-completion, and acts as a layer between visual studio
  * code and the DocBlockr parser
  */
 
-import { 
-  CancellationToken, 
-  CompletionItem, 
-  CompletionItemKind, 
-  CompletionItemProvider, 
-  Position, 
-  Range, 
-  SnippetString, 
-  TextDocument, 
+import {
+  CancellationToken,
+  CompletionItem,
+  CompletionItemKind,
+  CompletionItemProvider,
+  Position,
+  Range,
+  SnippetString,
+  TextDocument,
   window } from 'vscode';
 import { Parser } from './parser';
 
 /**
- * Snippet object used for auto-completion 
+ * Snippet object used for auto-completion
  */
 export interface Snippet {
   name:    string,
@@ -25,22 +25,22 @@ export interface Snippet {
 
 /**
  * Snippet handler
- * 
- * Used as a communication layer between visual studio code and the DocBlockr 
+ *
+ * Used as a communication layer between visual studio code and the DocBlockr
  * parser
  */
 export class Snippets implements CompletionItemProvider {
   /**
    * Language specific code parser
-   * 
-   * Handles code parsing, and generates docblock string. The language parser is 
+   *
+   * Handles code parsing, and generates docblock string. The language parser is
    * determined in `extension.ts`.
    */
   protected parser: Parser;
-  
+
   /**
    * Sets up the parser, instantiated from extension entry point
-   * 
+   *
    * @param  {parser}  parser  Code parser
    */
   public constructor(parser: Parser) {
@@ -49,36 +49,36 @@ export class Snippets implements CompletionItemProvider {
 
   /**
    * Gets word range at specified position
-   * 
+   *
    * Shortcut for `document.getWordRangeAtPosition`
-   * 
+   *
    * @param   {TextDocument}  document  TextDocument namespace
    * @param   {Position}      position  Position in the editor
    * @param   {RegExp}        regex     Expression to check against
-   * 
+   *
    * @return  {Range}                   Range of the matched text in the editor
    */
   private getWordRange(
-    document: TextDocument, 
-    position: Position, 
+    document: TextDocument,
+    position: Position,
     regex:    RegExp): Range {
     return document.getWordRangeAtPosition(position, regex);
   }
 
   /**
    * Listens for docblock characters, and sends code the `Parser`.
-   * 
-   * When `/**` is typed the `Parser`, specific to current language, is ran. 
-   * The code immediately below the cursor position is the parsed, and docblock 
+   *
+   * When `/**` is typed the `Parser`, specific to current language, is ran.
+   * The code immediately below the cursor position is the parsed, and docblock
    * string is returned.
-   * 
+   *
    * @param   {TextDocument}       document  TextDocument namespace
    * @param   {Position}           position  Position in the editor
-   * @param   {CancellationToken}  token     We aren't using this, but is 
-   *                                         required upon extending the 
+   * @param   {CancellationToken}  token     We aren't using this, but is
+   *                                         required upon extending the
    *                                         `CompletionItemProvider class
-   * 
-   * @return  {CompletionItem[]}             List of completion items for 
+   *
+   * @return  {CompletionItem[]}             List of completion items for
    *                                         auto-completion
    */
   public provideCompletionItems(
@@ -96,10 +96,10 @@ export class Snippets implements CompletionItemProvider {
       const item = new CompletionItem("/**", CompletionItemKind.Snippet);
       // Set word range within full docblock
       item.range = this.getWordRange(document, position, /\/\*\* \*\//);
-      // Parse the code below the current cursor position and return generated 
+      // Parse the code below the current cursor position and return generated
       // docblock string
       const docBlock = this.parser.init(window.activeTextEditor);
-      // In order for the snippet to display we need to convert it a snippet 
+      // In order for the snippet to display we need to convert it a snippet
       // string
       item.insertText = new SnippetString(docBlock);
       // Display details for docblock string
