57b7271e9220c8035df90b25da85a366be244bb7
Jeremy Jackson <jeremy@bluewall.com>
2018-09-21 14:53:27 -0500

    Fix variable parsing

--- test/languages/java.test.ts
+++ test/languages/java.test.ts
@@ -29,21 +29,22 @@ suite('Java', function () {
     });
 
     test('should parse function', function () {
-      let token = parser.tokenize('function foo() {');
+      let token = parser.tokenize('public void foo() {');
       assert.equal(token.name, 'foo');
       assert.equal(token.type, 'function');
       assert.equal(token.params.length, 0);
+      assert.equal(token.return.type, 'void');
       assert.equal(token.return.present, true);
     });
 
     test('should parse function with arguments', function () {
-      let token = parser.tokenize('function foo(arg1, arg2) {');
+      let token = parser.tokenize('public void foo(int arg1, int arg2) {');
       assert.equal(token.name, 'foo');
       assert.equal(token.type, 'function');
       assert.equal(token.params.length, 2);
       for (let i in token.params) {
         assert.equal(token.params[i].name, `arg${parseInt(i) + 1}`);
-        assert.equal(token.params[i].val, '');
+        assert.equal(token.params[i].val, 'int');
         assert.equal(token.params[i].type, undefined);
       }
       assert.equal(token.return.present, true);
