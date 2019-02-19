3554093fe5a4aa9d8a00c5c22a6818281d4db4a9
Jeremy Jackson <jeremy@bluewall.com>
2019-02-19 16:48:55 -0600

    Start implementation of Sass language

---
+++ src/languages/sass.ts
@@ -0,0 +1,65 @@
+/**
+ * Sass specific language parser
+ */
+
+'use strict';
+
+import { Param, Parser, Tokens } from '../parser';
+
+export class Sass extends Parser {
+  /**
+   * Constructs settings specific to Sass
+   */
+  constructor() {
+    super({
+      grammar: {
+        class: 'class',
+        function: 'function',
+        identifier: '[a-zA-Z_$0-9]',
+        modifiers: [],
+        types: [],
+        variables: [],
+      },
+    });
+  }
+
+  /**
+   * Create tokenized object based off of the output from the Lexer
+   *
+   * @param   {string}  code    Code to lex via the lexer
+   * @param   {string}  next    Token name from previous function instance. Used
+   *                            for letting the `tokenize` method now it should
+   *                            be expecting a token name
+   * @param   {mixed}   tokens  Tokens created from the previous tokenize
+   *                            instance
+   *
+   * @return  {Tokens}          Tokens retrieved from Lexer output
+   */
+  public tokenize(code: string, next: string = '', tokens: Tokens = null): Tokens {
+    // Create empty token object if none is present
+    if (tokens === null) {
+      tokens = {name: '', type: '', params: [], return: { present: true }};
+    }
+    // Don't continue unless we have workable value
+    if (code !== undefined) {
+      // Separate code string with lexer
+      const lexed = this.lex(code); console.log(lexed);
+      // The initial lexed object is the result of what was lexed
+      const result = lexed[0];
+      // The lexed object with the text type is what is next to be lexed
+      const text = this.findByType('text', lexed);
+      // Get end of line position
+      const eos = this.findByType('eos', lexed);
+      // Check if the end of the line has been reached
+      if (text && text.col < eos.col) {
+        // Create new regular expression object based on grammar identifier
+        const cleanExp = new RegExp('^' + this.settings.grammar.identifier);
+        // Make sure we aren't about to lex malformed input
+        if (cleanExp.test(text.val.substr(0, 1))) {
+          // Continue the lexing process and the data up next
+          this.tokenize(text.val, next, tokens);
+        }
+      }
+    }
+  }
+}
