971a104bbd80807b8e0af6c49bde903bc1158991
Jeremy Jackson <git@jeremyvii.com>
2019-10-09 17:24:20 -0500

    Remove debug code

--- test/runTest.ts
+++ test/runTest.ts
@@ -15,7 +15,6 @@ async function main() {
     // Download VS Code, unzip it and run the integration test
     await runTests({ extensionDevelopmentPath, extensionTestsPath });
   } catch (err) {
-    console.log(err);
     console.error('Failed to run tests');
     process.exit(1);
   }
