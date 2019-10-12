8250f031d220c03d5601655a8775dbd44a2432c7
Jeremy Jackson <git@jeremyvii.com>
2019-10-11 22:35:21 -0500

    Continue C language
    * Create token class
    * Add regex for parsing C functions

--- src/languages/c.ts
+++ src/languages/c.ts
@@ -4,7 +4,8 @@
 
 'use strict';
 
-import { Parser, Param, Tokens } from '../parser';
+import { Tokens } from '../tokens';
+import { Parser } from '../parser';
 
 export class C extends Parser {
   /**
@@ -15,32 +16,79 @@ export class C extends Parser {
       grammar: {
         function: 'function',
         class: 'class',
-        identifier: '[a-zA-Z_][a-zA-Z0-9_]*',
+        identifier: '^[a-zA-Z_][a-zA-Z0-9_]*$',
         modifiers: [],
         variables: [],
-        types: [],
+        types: [
+          'char',
+          'double',
+          'float',
+          'int',
+          'short',
+          'long',
+          'void',
+        ],
       }
     });
   }
 
   /**
    * Create tokenized object based off of the output from the Lexer
-   * 
+   *
    * @param   {string}  code    Code to lex via the bug lexer
    * @param   {string}  next    Token name from previous function instance. Used
    *                            for letting the `tokenize` method now it should
    *                            be expecting a token name
    * @param   {mixed}   tokens  Tokens created from the previous tokenize
    *                            instance
-   * 
+   *
    * @return  {Tokens}          Tokens retrieved from Lexer output
    */
-  public tokenize(code: string, next: string = '', tokens: Tokens = null): Tokens {
-    // Create empty token object if none is present
-    if (tokens === null) {
-      tokens = {name: '', type: '', params: [], return: { present: true }};
+  public tokenize(
+    code: string,
+    next: string = '',
+    tokens: Tokens = new Tokens(),
+  ): Tokens {
+    // Don't continue unless we have a workable value
+    if (code !== undefined) {
+      // Short cut to valid variable name
+      const ident = this.settings.grammar.identifier;
+      const types = this.settings.grammar.types;
+      // Lex code string provided
+      const lexed = this.lex(code);
+      // The initial lexed object is the result of what was lexed
+      const result = lexed[0];
+      // The lexed object with the text type is what is next to be lexed
+      const text = this.findByType('text', lexed);
+      // Get end of line position
+      const eos = this.findByType('eos', lexed);
+
+      const cExp = new RegExp(`^\\s*(unsigned|signed)?\\s*(${types.join('|')})\\s+(\\w+)\\s*\\([^)]*\\)\\s*\\{`);
+
+      if (cExp.test(code)) {
+        const functionResults = cExp.exec(code);
+
+        tokens.type = functionResults[2];
+        tokens.name = functionResults[3];
+
+      }
+
+      // Check if the end of the line has been reached
+      if (text && text.col < eos.col) {
+        // Create new regular expression object based on grammar identifier
+        const regex = new RegExp(`^[${ident}]`);
+        // Make sure we aren't about to lex malformed input
+        if (regex.test(text.val.substr(0, 1))) {
+          // Continue the lexing process and the data up next
+          this.tokenize(text.val, next, tokens);
+        }
+      }
+      console.log([
+        result, text, eos,
+      ]);
     }
-    console.log(this.lex(code));
+
+    console.log(tokens);
     return tokens;
   }
 }
--- src/parser.ts
+++ src/parser.ts
@@ -17,71 +17,9 @@
 
 import { Lexed, Lexer } from './lexer';
 import { Options, Settings } from './settings';
+import { Tokens } from './tokens';
 
 import * as vscode from 'vscode';
-
-/**
- * Describes a function parameter
- */
-export interface Param {
-  /**
-   * Parameter's name. Should always be present
-   */
-  name: string;
-
-  /**
-   * Parameter's value. Usually empty string if no parameter value is provided
-   */
-  val: string;
-
-  /**
-   * Parameter's data type. This is usually language specific and is not
-   * required. Ex. string, integer, array, etc.
-   */
-  type?: string;
-}
-
-/**
- * Tokenized code returned from lexer. This defines whether lexed code was a
- * class, function of variable
- */
-export interface Tokens {
-  /**
-   * Name of code binding/identifier
-   */
-  name: string;
-
-  /**
-   * What type of code it is. (class, function, variable)
-   */
-  type: string;
-
-  /**
-   * When `Tokens.type` is variable, this optional value refers to that
-   * variables type
-   */
-  varType?: string;
-
-  /**
-   * Describes if there is a return value, and what type it is
-   */
-  return?: {
-    /**
-     * Indicates if there is a return value for this function
-     */
-    present: boolean;
-
-    /**
-     * Describes what type of return value. (Optional)
-     */
-    type?: string;
-  };
-
-  /**
-   * List of function parameters if token is describing a function
-   */
-  params?: Param[];
-}
 
 /**
  * Initial Class for parsing Doc Block comments
