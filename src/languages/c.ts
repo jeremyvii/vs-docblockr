a75452410de80171dfd66d16978e9e7148b79a0e
Jeremy Jackson <git@jeremyvii.com>
2019-10-25 17:08:24 -0500

    Debug c language

--- src/languages/c.ts
+++ src/languages/c.ts
@@ -4,6 +4,7 @@
 
 'use strict';
 
+import { Lexed } from '../lexer';
 import { Parser } from '../parser';
 import { Param, Tokens } from '../tokens';
 
@@ -14,12 +15,18 @@ export class C extends Parser {
   constructor() {
     super({
       grammar: {
-        class: 'class',
-        function: 'function',
+        class: 'struct',
         identifier: '^[a-zA-Z_][a-zA-Z0-9_]*$',
         modifiers: [
           'unsigned',
           'signed',
+          'struct',
+          'static',
+          'inline',
+          'const',
+          'auto',
+          'extern',
+          'complex',
         ],
         types: [
           'char',
@@ -55,39 +62,53 @@ export class C extends Parser {
     // Don't continue unless we have a workable value
     if (code !== undefined) {
       // Short cut to valid variable name
-      const ident = this.settings.grammar.identifier;
       const types = this.settings.grammar.types;
       const mods = this.settings.grammar.modifiers;
-      // Lex code string provided
-      let lexed = this.lex(code);
-      // The lexed object with the text type is what is next to be lexed
-      const text = this.findByType('text', lexed);
-      // Get end of line position
-      const eos = this.findByType('eos', lexed);
 
-      const cExp = new RegExp(`^\\s*(${mods.join('|')})?\\s*(${types.join('|')})\\s+(\\w+)\\s*\\(([^)]*)\\)\\s*\\{`);
-      if (cExp.test(code)) {
-        const functionResults = cExp.exec(code);
+      let lexed: Lexed[];
 
-        tokens.type = functionResults[2];
+      // Define an expression to matches C functions
+      const functionPattern = `^\\s*(${mods.join('|')})?\\s*(${types.join('|')})\\s+(\\w+)\\s*\\(([^)]*)\\)\\s*\\{?`;
+      const functionExp = new RegExp(functionPattern);
+      // Test if the current code matches the function regular expression
+      if (functionExp.test(code)) {
+        // Get an array of function pieces based on the expression
+        const functionResults = functionExp.exec(code);
+
+        // Guess the functions name and return type based on the matches
         tokens.name = functionResults[3];
+        tokens.type = 'function';
+        tokens.return.type = functionResults[2];
 
+        // Fetch the functions parameters, if any
         const tempParams = functionResults[4];
 
+        // Determine if any parameters were found
         if (tempParams) {
           lexed = this.lex(`(${tempParams})`);
 
+          // Check if the lexer parsed any attributes.
           if (this.findByType('start-attributes', lexed)) {
+            // Create a placeholde variable for the parameter's type, since it
+            // preceeds the parameter's name
             let paramType = '';
+
             for (const i in lexed) {
+              // Only operate on items with the attribute type
               if (lexed[i].type === 'attribute') {
+                // Check if the current item matches any of the parameter types
                 if (this.matchesGrammar(lexed[i].name, 'types')) {
                   paramType = lexed[i].name;
+                } else if (this.matchesGrammar(lexed[i].name, 'modifiers')) {
+                  // Skip any modifiers that may exist in the arguments
                 } else {
+                  // Set the parameter's name
                   const param: Param = {
                     name: lexed[i].name,
                     val: '',
                   };
+
+                  // Set the parameter's type if there is any
                   if (paramType) {
                     param.type = paramType;
                   }
@@ -97,22 +118,35 @@ export class C extends Parser {
               }
             }
           }
+
+          return tokens;
         }
       }
 
-      // Check if the end of the line has been reached
-      if (text && text.col < eos.col) {
-        // Create new regular expression object based on grammar identifier
-        const regex = new RegExp(`^[${ident}]`);
-        // Make sure we aren't about to lex malformed input
-        if (regex.test(text.val.substr(0, 1))) {
-          // Continue the lexing process and the data up next
-          this.tokenize(text.val, next, tokens);
-        }
+      const structExp = new RegExp(/^(typedef)?\s?struct\s?([\w]*)?{?/);
+
+      if (structExp.test(code)) {
+        tokens.name = 'struct';
+        tokens.return.present = false;
+
+        return tokens;
+      }
+
+      const varMods = mods.map(mod => `${mod}\\s*`);
+
+      const varExp = new RegExp(`/\\b(?:(?:${varMods.join('|')})+)(?:\\s+\\*?\\*?\\s*)([a-zA-Z_][a-zA-Z0-9_]*)\\s*[\\[;,=)]/`);
+
+      // @TODO: new RegExp(/\b(?:(?:extern\s*|auto\s*)*)(?:\s+\*?\*?\s*)(int|float)+\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*[\[;,=)]/);
+
+      console.log(varExp.source);
+
+      if (varExp.test(code)) {
+        const varResults = varExp.exec(code);
+
+        console.log(varResults);
       }
     }
 
-    // console.log(tokens);
     return tokens;
   }
 }
