d077240b8b4af79b94a313e2ef76204a89cf9c86
Jeremy Jackson <git@jeremyvii.com>
2019-10-26 15:07:03 -0500

    Update unit test's code style

--- test/languages/java.test.ts
+++ test/languages/java.test.ts
@@ -6,12 +6,13 @@ import * as assert from 'assert';
 import { Java } from '../../src/languages/java';
 
 // Get parser instance
-let parser = new Java();
+const parser = new Java();
 
-suite('Java', function () {
-  suite('tokenize', function () {
-    test('should parse variable', function () {
-      let token = parser.tokenize('int foo = 5;');
+suite('Java', () => {
+  suite('tokenize', () => {
+    test('should parse variable', () => {
+      const token = parser.tokenize('int foo = 5;');
+
       assert.equal(token.name, 'foo');
       assert.equal(token.type, 'variable');
       assert.equal(token.varType, 'int');
@@ -19,8 +20,9 @@ suite('Java', function () {
       assert.equal(token.return.present, false);
     });
 
-    test('should parse undefined variable', function () {
-      let token = parser.tokenize('boolean foo;');
+    test('should parse undefined variable', () => {
+      const token = parser.tokenize('boolean foo;');
+
       assert.equal(token.name, 'foo');
       assert.equal(token.type, 'variable');
       assert.equal(token.varType, 'boolean');
@@ -28,8 +30,9 @@ suite('Java', function () {
       assert.equal(token.return.present, false);
     });
 
-    test('should parse function', function () {
-      let token = parser.tokenize('public void foo() {');
+    test('should parse function', () => {
+      const token = parser.tokenize('public void foo() {');
+
       assert.equal(token.name, 'foo');
       assert.equal(token.type, 'function');
       assert.equal(token.params.length, 0);
@@ -37,21 +40,25 @@ suite('Java', function () {
       assert.equal(token.return.present, true);
     });
 
-    test('should parse function with arguments', function () {
-      let token = parser.tokenize('public void foo(int arg1, int arg2) {');
+    test('should parse function with arguments', () => {
+      const token = parser.tokenize('public void foo(int arg1, int arg2) {');
+
       assert.equal(token.name, 'foo');
       assert.equal(token.type, 'function');
       assert.equal(token.params.length, 2);
-      for (let i in token.params) {
-        assert.equal(token.params[i].name, `arg${parseInt(i) + 1}`);
-        assert.equal(token.params[i].val, '');
-        assert.equal(token.params[i].type, 'int');
+      for (const i in token.params) {
+        if (token.params[i]) {
+          assert.equal(token.params[i].name, `arg${parseInt(i) + 1}`);
+          assert.equal(token.params[i].val, '');
+          assert.equal(token.params[i].type, 'int');
+        }
       }
       assert.equal(token.return.present, true);
     });
 
-    test('should parser function with multiple modifiers', function () {
-      let token = parser.tokenize('public static void foo() {');
+    test('should parser function with multiple modifiers', () => {
+      const token = parser.tokenize('public static void foo() {');
+
       assert.equal(token.name, 'foo');
       assert.equal(token.type, 'function');
       assert.equal(token.params.length, 0);
@@ -59,16 +66,18 @@ suite('Java', function () {
       assert.equal(token.return.present, true);
     });
 
-    test('should parse class', function () {
-      let token = parser.tokenize('class Bar {');
+    test('should parse class', () => {
+      const token = parser.tokenize('class Bar {');
+
       assert.equal(token.name, 'Bar');
       assert.equal(token.type, 'class');
       assert.equal(token.params.length, 0);
       assert.equal(token.return.present, false);
     });
 
-    test('should parse abstract class', function () {
-      let token = parser.tokenize('abstract class Bar {');
+    test('should parse abstract class', () => {
+      const token = parser.tokenize('abstract class Bar {');
+
       assert.equal(token.name, 'Bar');
       assert.equal(token.type, 'class');
       assert.equal(token.params.length, 0);
