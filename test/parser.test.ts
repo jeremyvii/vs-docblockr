4fd8a014d7e05a459b1298e4a7882a97256c270e
Jeremy Jackson <git@jeremyvii.com>
2020-03-23 15:16:58 -0500

    Test comment style

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -2,12 +2,12 @@ import * as assert from 'assert';
 import { TypeScript } from '../src/languages/typescript';
 import { Tokens } from '../src/tokens';
 
+// Use the JavaScript parser for the sake of setup
+const parser = new TypeScript();
+
 suite('Parser', () => {
   suite('renderBlock', () => {
     test('should return docblock without trailing whitespace', () => {
-      // Use the JavaScript parser for the sake of setup
-      const parser = new TypeScript();
-
       const token: Tokens = {
         name: 'foo',
         params: [{
@@ -24,6 +24,28 @@ suite('Parser', () => {
 
       const block = parser.renderBlock(token);
       assert.equal(/\s$/gm.test(block), false, 'No trailing whitespace');
+    });
+
+    test('should use drupal comment style when configured', () => {
+      parser.style = 'drupal';
+
+      const token = parser.tokenize('function foo(bar) {');
+
+      const result = parser.renderBlock(token);
+
+      const expected = [
+        '/**',
+        ' * ${1:[foo description]}',
+        ' *',
+        ' * @param {${2:[type]}} bar',
+        ' *   ${3:[bar description]}',
+        ' *',
+        ' * @return {${4:[type]}}',
+        ' *   ${5:[return description]}',
+        ' */',
+      ].join('\n');
+
+      assert.equal(result, expected);
     });
   });
 });
