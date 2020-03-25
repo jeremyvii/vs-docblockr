0e8efc67ac64b9570da450aac1a47a43fed56dcc
Jeremy Jackson <git@jeremyvii.com>
2020-03-25 08:09:07 -0500

    Properly exit tests

--- test/runTest.ts
+++ test/runTest.ts
@@ -15,7 +15,11 @@ async function main() {
     // Download VS Code, unzip it and run the integration test
     await runTests({ extensionDevelopmentPath, extensionTestsPath });
   } catch (err) {
-    throw new Error('Failed to run tests');
+    // tslint:disable-next-line: no-console
+    console.error(err);
+    // tslint:disable-next-line: no-console
+    console.error('Failed to run tests');
+    process.exit(1);
   }
 }
 
