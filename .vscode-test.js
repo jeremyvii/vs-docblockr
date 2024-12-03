5115fc0997576906bd0a4b685bdc622a85d151ca
Jeremy Jackson <jeremy@librarymarket.com>
2024-12-03 12:03:36 -0600

    Update timeout

--- .vscode-test.js
+++ .vscode-test.js
@@ -3,4 +3,7 @@ const { defineConfig } = require('@vscode/test-cli');
 module.exports = defineConfig([{
   extensionDevelopmentPath: './',
   files: 'out/test/**/*.js',
+  mocha: {
+    timeout: 5000,
+  },
 }]);
